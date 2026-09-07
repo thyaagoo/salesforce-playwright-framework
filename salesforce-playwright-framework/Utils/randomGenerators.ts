0a42023151ea9e8d46da8497dc839bad24530193
thyaagoo <thyaagoo@gmail.com>
2026-09-07 17:58:10 +0530

    cleared duplicate files

--- salesforce-playwright-framework/Utils/randomGenerators.ts
+++
@@ -1,8 +0,0 @@
-export function generateRandomPhone(): string {
-  return String(Math.floor(1000000 + Math.random() * 9000000));
-}
-
-export function generateRandomWebsite(): string {
-  const randomText = Math.random().toString(36).substring(2, 8);
-  return `www.${randomText}-${Date.now()}.com`;
-}
