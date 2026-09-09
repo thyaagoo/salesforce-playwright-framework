23c98dfcc71a030f202da13b486c4f2a10986d8f
thyaagoo <thyaagoo@gmail.com>
2026-09-09 14:01:29 +0530

    Minor changes to Json entries

--- tests/TC001_create_account.spec.ts
+++ tests/TC001_create_account.spec.ts
@@ -1,17 +1,17 @@
 import { test } from "../custom_fixture/salesforce.fixture";
 import accountsJSONInput from "../Data/create_account_data.json";
 
-for (const account of accountsJSONInput) {
-  test(`Create new account - ${account.accountName}`, async ({ accountsPage }) => {
-    await accountsPage.loadURL();
-    await accountsPage.clickAccountsModule();
-    await accountsPage.clickNewAccount();
-    await accountsPage.fillAccountForm(account);
-    await accountsPage.clickSaveAccountBtn();
-    await accountsPage.assertAccountCreation();
+const account = accountsJSONInput[0];
 
-    await accountsPage.uploadFile("Data/samplefile.txt");
-    await accountsPage.clickUploadDoneButton();
-    await accountsPage.assertFileUploaded();
-  });
-}
+test(`Create new account - ${account.accountName}`, async ({ accountsPage }) => {
+  await accountsPage.loadURL();
+  await accountsPage.clickAccountsModule();
+  await accountsPage.clickNewAccount();
+  await accountsPage.fillAccountForm(account);
+  await accountsPage.clickSaveAccountBtn();
+  await accountsPage.assertAccountCreation();
+
+  await accountsPage.uploadFile("Data/samplefile.txt");
+  await accountsPage.clickUploadDoneButton();
+  await accountsPage.assertFileUploaded();
+});
--- tests/TC003_delete_account.spec.ts
+++ tests/TC003_delete_account.spec.ts
@@ -1,15 +1,15 @@
 import { test } from "../custom_fixture/salesforce.fixture";
 import deleteAccountJSON from "../Data/delete_account_data.json";
 
-for (const account of deleteAccountJSON) {
-  test(`Delete account - ${account.accountName}`, async ({ accountsPage }) => {
-    await accountsPage.loadURL();
-    await accountsPage.clickAccountsModule();
-    await accountsPage.clickNewAccount();
-    await accountsPage.fillAccountForm(account);
-    await accountsPage.clickSaveAccountBtn();
-    await accountsPage.assertAccountCreation();
-    await accountsPage.deleteCreatedAccount();
-    await accountsPage.assertAccountDeleted();
-  });
-}
+const account = deleteAccountJSON[0];
+
+test(`Delete account - ${account.accountName}`, async ({ accountsPage }) => {
+  await accountsPage.loadURL();
+  await accountsPage.clickAccountsModule();
+  await accountsPage.clickNewAccount();
+  await accountsPage.fillAccountForm(account);
+  await accountsPage.clickSaveAccountBtn();
+  await accountsPage.assertAccountCreation();
+  await accountsPage.deleteCreatedAccount();
+  await accountsPage.assertAccountDeleted();
+});
